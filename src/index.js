562adc3b748c92270609fc8cca90a436880f9357
susanlin0705 <susan.lin0705@gmail.com>
2019-07-20 10:23:28 -0500

    complete On page load:

--- src/index.js
+++ src/index.js
@@ -8,13 +8,30 @@ sayHello('World');
  * require style imports
  */
 const {getMovies} = require('./api.js');
+const $ = require('jquery');
+
+
+$(".container").css("background-color","pink");
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
+  let storeMovies="";
   movies.forEach(({title, rating, id}) => {
+
+    storeMovies+=`<div class="title">Title: ${title} </div>`;
+    storeMovies+=`<div class="rating">Rating: ${rating} </div>`;
+    storeMovies+=`<div class="id">ID: ${id} </div>`;
+
     console.log(`id#${id} - ${title} - rating: ${rating}`);
   });
+  $('#movies').html(storeMovies);
 }).catch((error) => {
-  alert('Oh no! Something went wrong.\nCheck the console for details.')
+  alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
+
+$("#movies").hide();
+$("#loading").fadeOut(3000,function () {
+  $("#movies").show();
+  $("#movies").fadeIn.text(storeMovies)
+})
