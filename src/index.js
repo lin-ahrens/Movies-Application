f5e0cc3d3f8d143096826826cf1fe8bd40c4ae6b
susanlin0705 <susan.lin0705@gmail.com>
2019-07-20 10:29:21 -0500

    change the fadein and fadeout inside the themovies.then

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ const $ = require('jquery');
 
 
 $(".container").css("background-color","pink");
+$("#movies").hide();
 
 getMovies().then((movies) => {
   console.log('Here are all the movies:');
@@ -25,13 +26,16 @@ getMovies().then((movies) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
   });
   $('#movies').html(storeMovies);
+  $("#loading").fadeOut(2000,function () {
+    $("#movies").show();
+    $("#movies").fadeIn.text(storeMovies)
+  })
+
+
+//放置在then裡面能確保執行時畫面能同步
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.');
   console.log(error);
 });
 
-$("#movies").hide();
-$("#loading").fadeOut(3000,function () {
-  $("#movies").show();
-  $("#movies").fadeIn.text(storeMovies)
-})
+
