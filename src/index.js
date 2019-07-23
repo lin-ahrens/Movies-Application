9f79115a28e1a612dbf361ac61d1bb2ad651fee3
susanlin0705 <susan.lin0705@gmail.com>
2019-07-22 20:12:43 -0500

    sort function

--- src/index.js
+++ src/index.js
@@ -15,6 +15,57 @@ const $ = require('jquery');
 $(".container").css("background-color","pink");
 
 
+// =============== Remove Movie================
+function reRun() {
+  $(".remove").click((event) => {
+    const id = event.target.value;
+
+    console.log("Click button yay", id);
+    deleteMovie(id)
+        .then((response) => {
+          console.log("movie deleted")
+          loaded()
+        }).catch(() => {
+      console.log("not working! error")
+    });
+  });
+
+// =============== Edit Movie================
+
+  $(".edit").click((event) => {
+    const id = event.target.value;
+    // const newTitle = $(`.movie-container-${id} .title`).data("value");
+    // const newRating = $(`.movie-container-${id} .rating`).data("value");
+    const newTitle = $(`.movie-container-${id} .title`).val();
+    const newRating = $(`.movie-container-${id} .rating`).val();
+    const newGenre = $(`.movie-container-${id} .genre`).val();
+    console.log("edit movie", id);
+    $(`.movie-container-${id} .title`).html(`Title: <input id="edit-title-${id}" value="${newTitle}">`)
+    $(`.movie-container-${id} .rating`).html(`Rating: <input id="edit-rating-${id}"  value="${newRating}">`)
+    $(`.movie-container-${id} .genre`).html(`Genre: <input id="edit-genre-${id}"  value="${newGenre}">`)
+    // $(`.movie-container-${id} .edit`).hide()
+  });
+
+// =============== Change Movie================
+  $(".change").click((event) => {
+    const id = event.target.value;
+    const newTitleDataVersion = $(`.movie-container-${id} .title`).data("value");
+
+    // const newTitle = $(`.movie-container-${id} .title`).val();
+    const editTitle = $(`#edit-title-${id}`).val();
+    // const newRating =  $(`.movie-container-${id} .rating`).val();
+    const editRating = $(`#edit-rating-${id}`).val();
+    const editGenre = $(`#edit-genre-${id}`).val()
+    editMovie(id, editTitle, editRating, editGenre)
+        .then((response) => {
+          console.log("movie edit", id);
+          loaded()
+        }).catch(() => {
+      console.log("edit movie not working! error")
+    })
+  })
+}
+
 function loaded() {
   $("#movies").hide();
   getMovies().then((movies) => {
@@ -34,62 +85,18 @@ function loaded() {
 
       console.log(`id#${id} - ${title} - rating: ${rating} - genre: ${genre}`);
 
-
+    reRun()
     });
+
 
 // =============== On Load================
     $('#movies').html(storeMovies);
+    reRun()
     $("#loading").fadeOut(2000, function () {
       $("#movies").show();
     })
 
-// =============== Remove Movie================
-    $(".remove").click((event) => {
-      const id =  event.target.value;
 
-      console.log("Click button yay", id);
-      deleteMovie(id)
-          .then((response) => {
-            console.log("movie deleted")
-          loaded()
-          }).catch(() => {
-        console.log("not working! error")
-      });
-    });
-
-// =============== Edit Movie================
-    $(".edit").click((event)=>{
-      const id = event.target.value;
-      // const newTitle = $(`.movie-container-${id} .title`).data("value");
-      // const newRating = $(`.movie-container-${id} .rating`).data("value");
-      const newTitle = $(`.movie-container-${id} .title`).val();
-      const newRating = $(`.movie-container-${id} .rating`).val();
-      const newGenre = $(`.movie-container-${id} .genre`).val();
-      console.log("edit movie",id);
-      $(`.movie-container-${id} .title`).html(`Title: <input id="edit-title-${id}" value="${newTitle}">`)
-      $(`.movie-container-${id} .rating`).html(`Rating: <input id="edit-rating-${id}"  value="${newRating}">`)
-      $(`.movie-container-${id} .genre`).html(`Genre: <input id="edit-genre-${id}"  value="${newGenre}">`)
-      // $(`.movie-container-${id} .edit`).hide()
-    });
-
-// =============== Change Movie================
-    $(".change").click((event)=>{
-      const id = event.target.value;
-      const newTitleDataVersion = $(`.movie-container-${id} .title`).data("value");
-
-      // const newTitle = $(`.movie-container-${id} .title`).val();
-      const editTitle =$(`#edit-title-${id}`).val();
-      // const newRating =  $(`.movie-container-${id} .rating`).val();
-      const editRating = $(`#edit-rating-${id}`).val();
-      const editGenre = $(`#edit-genre-${id}`).val()
-      editMovie(id, editTitle,editRating,editGenre)
-          .then((response)=>{
-            console.log("movie edit",id);
-            loaded()
-          }).catch(()=>{
-            console.log("edit movie not working! error")
-      })
-    })
 
 //放置在then裡面能確保執行時畫面能同步
   }).catch((error) => {
@@ -119,24 +126,37 @@ $("#clickSearchTitle").click(function () {
   getMovies().then((movies) => {
     let storeMovies = "";
 
-    const sortArray =
+    // const sortArray =
         movies.sort(function (a, b) {
-          if (a.title < b.title) {
+          if (a.title.toLowerCase() < b.title.toLowerCase()) {
             return -1
-          } else if (a.title > b.title) {
+          } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
             return 1
           } else {
             return 0
           }
-        })
+        });
+        console.log(movies);
 
 
-    movies.forEach(({title}) => {
+    movies.forEach(({title, rating, genre, id}) => {
 
       storeMovies += `<div class="movie-container-${id}">`;
       storeMovies += `<div class="title" data-value="${title}">Title: ${title} </div>`;
+      storeMovies += `<div class="rating" data-value="${rating}">Rating: ${rating} </div>`;
+      storeMovies += `<div class="genre" data-value="${genre}">Category: ${genre} </div>`;
+      storeMovies += `<button class="remove" id="remove-${id}" value="${id}">Remove</button></div>`;
+      storeMovies += `<button class="edit" id="edit-${id}" value="${id}">Edit Movie</button>`;
+      storeMovies += `<button class="change" id="change-${id}" value="${id}">Change Movie</button>`;
+      storeMovies += `</div>`;
+
+      console.log(`id#${id} - ${title} - rating: ${rating} - genre: ${genre}`);
+
 
     })
+    $('#movies').html(storeMovies);
+
+    reRun()
   })
 })
   console.log("search show")
